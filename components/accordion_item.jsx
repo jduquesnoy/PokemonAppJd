d3cd6367725dabf06261d97481e2c261ab028aca
Jocelyn Duquesnoy <jocelyn.duquesnoy@yahoo.fr>
2021-08-29 11:23:35 +0200

    cdectecting screen rotation

--- components/accordion_item.jsx
+++ components/accordion_item.jsx
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
-import {Text, View, StyleSheet, Image} from 'react-native';
+import {Text, View, StyleSheet, Image, Dimensions} from 'react-native';
 // require('../assets/#001.png')
 class AccordionItem extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      isActive: false,
+      screenHeight: null,
+      screenWidth: null
+    }
+  }
+
+  handlePokemon = () =>{
+    this.setState(prevState =>({isActive: !prevState.isActive}));
+  }
+
+  handlelayout = (e) =>{
+    console.log("Screen Orientation Changed...")
+    this.setState({
+      screenWidth: Dimensions.get('window').width,
+      screenHeight: Dimensions.get('window').height
+    })
+  }
+
 
   render(){
     const pokemonSource = require(`../assets/${this.props.pokemon.photo}.png`);
     return (
       <View>
-        <Image
-          style={styles.pokemonImage}
-          source={pokemonSource}
-        />
-        <Text>N° pokedex: {this.props.pokemon.pokedex_number}</Text>
-        <Text>Nom version française: {this.props.pokemon.french_name}</Text>
-        <Text>Nom version anglaise: {this.props.pokemon.english_name}</Text>
-        <Text>Nom version japonaise: {this.props.pokemon.japanese_name}</Text>
-        <Text>Description: {this.props.pokemon.description}</Text>
+        <View>
+          <Text
+            onPress={this.handlePokemon}>
+            {this.props.pokemon.french_name}
+          </Text>
+          <Text>{this.state.isActive ? '-' : '+'}</Text>
+        </View>
+        { this.state.isActive &&
+        <View>
+            <Image
+              style={styles.pokemonImage}
+              source={pokemonSource}
+            />
+          <View style={this.state.screenHeight > this.state.screenWidth ? styles.ContainerPortrait : styles.ContainerLandscape} onLayout={this.handlelayout}>
+            <Text>N° pokedex: {this.props.pokemon.pokedex_number}</Text>
+            <Text>Nom version anglaise: {this.props.pokemon.english_name}</Text>
+            <Text>Nom version japonaise: {this.props.pokemon.japanese_name}</Text>
+            <Text>Description: {this.props.pokemon.description}</Text>
+          </View>
+        </View>
+        }
       </View>
     );
   }
@@ -25,6 +58,18 @@ const styles = StyleSheet.create({
   pokemonImage: {
     with: 250,
     height: 250,
+  },
+  ContainerPortrait: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  ContainerLandscape: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   // titleText: {
   //   fontSize: 20,
